ef0966504b190d4a4ee4c60e50de7c1c31ce4668
gui656 <guilhermeteodoro656@gmail.com>
2024-01-15 15:24:23 -0300

    Overhaul to the project

---
+++ script.js
@@ -0,0 +1,36 @@
+const display = document.querySelector('.display')
+const button = document.querySelectorAll('.btn')
+let operatorActive = false;
+let maxLength = 12
+
+const displayValue = (value) => {
+    if(display.textContent.length !== maxLength){
+        display.textContent += value
+        operatorActive = false
+    } else {
+        calculate()
+    }
+}
+
+const operatorBtn = (operator) => {
+    if(display.textContent !== '' && display.textContent.length !== maxLength){
+       if(!operatorActive){
+            display.textContent += operator
+            operatorActive = true 
+        }    
+    }    
+}
+
+const clearDisplay = function (){
+    display.textContent = ''
+    operatorActive = false
+}
+
+function calculate() {
+    try {
+        let result = math.evaluate(display.innerText);
+        display.innerText = (result === undefined || result === "") ? "Error" : result;
+    } catch (error) {
+        display.innerText = "Error";
+    }
+}
